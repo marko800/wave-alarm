5130224a31e54f50c4ce3690680751740c47655b
Marko Berghoff <berghoffmj@gmail.com>
2024-12-17 08:30:41 +0100

    fix deploy issue2

--- app/Home.py
+++ app/Home.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# add project root directory to Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
+
 import streamlit as st
 import pandas as pd
 
