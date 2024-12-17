93d97b5e146e40a49147dc5c6a7b7857a3660bda
Marko Berghoff <berghoffmj@gmail.com>
2024-12-17 08:38:45 +0100

    test: fix setup.py and requirements.txt

--- app/Home.py
+++ app/Home.py
@@ -1,9 +1,3 @@
-import sys
-import os
-
-# add project root directory to Python path
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
-
 import streamlit as st
 import pandas as pd
 
