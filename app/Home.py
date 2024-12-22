d451a2709cc1790b86d1c797cd57a6b6900cd21a
Marko Berghoff <berghoffmj@gmail.com>
2024-12-22 16:55:24 +0100

    changed criteria for surf (longer wind window), displayed data - couldn't test properly because of bad forecast...

--- app/Home.py
+++ app/Home.py
@@ -48,18 +48,32 @@ if st.button('Get pitted'):
         # set alarm counter
         alarm = 0
 
-        # check conditions (wind speed/gusts > 25/30 km/h, correct wind direction)
-        for row, index in data.iterrows():
-            if (index["wind_speed (km/h)"] > 25) and (index["wind_gusts (km/h)"] > 30) and (
-                spots_dict[spot]["wind_window"][0] <= index["wind_deg (°)"] <= spots_dict[spot]["wind_window"][1]):
+        # if surf is found, we display only relevant rows of the forecast
+        surf_rows = []
+
+        # check conditions (wind speed/gusts > 25/30 km/h, correct wind direction) in two consecutive rows
+        for row in range(len(data)-1):
+            current_row = data.iloc[row]
+            next_row = data.iloc[row + 1]
+
+            if (current_row["wind_speed (km/h)"] > 25) and (current_row["wind_gusts (km/h)"] > 30) and (
+                spots_dict[spot]["wind_window"][0] <= current_row["wind_deg (°)"] <= spots_dict[spot]["wind_window"][1]) and (
+                next_row["wind_speed (km/h)"] > 25) and (next_row["wind_gusts (km/h)"] > 30) and (
+                spots_dict[spot]["wind_window"][0] <= next_row["wind_deg (°)"] <= spots_dict[spot]["wind_window"][1]
+                ):
                 alarm = 1
-        # if any surf is found, display the forecast
+                surf_rows.append(row)
+
+        # if any surf is found, display the relevant part of the forecast
         if alarm == 1:
             #st.dataframe(data.style.apply(forecast.color_rows, axis=1,args = (spot,spots_dict)),
             #             width=2000, height=320)
             st.write(f"Yeewww, surf's up in {spot}:")
-            st.table(data.style.apply(forecast.color_rows, axis=1,args = (spot,spots_dict)))
+            for row in surf_rows:
+                surrounding_rows = data.iloc[max(0, row - 2) : min(len(data), row + 3)]
+                st.table(surrounding_rows.style.apply(forecast.color_rows, axis=1, args=(spot, spots_dict)))
+                
         # if nothing is found, print a negative message
-        if alarm == 0:
+        else:
             st.write(f"Nope, nothing on the horizon for {spot}.")
             st.write("")
